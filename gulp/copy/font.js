c0a765b075e733c8b5c484019153539cc4725a16
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2016-04-26 16:49:19 +0300

    Update 2016-04-26T13:49:03.850Z

--- gulp/copy/font.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-  var gulp = this.gulp;
-  var config = this.opts.config;
-  return gulp.src('.bower/bower/lembas/dist/fonts/**')
-    .pipe(gulp.dest(config.dist + '/fonts'));
-};
