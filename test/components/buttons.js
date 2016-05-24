bb9ab843d2710fdb984f5bd3da13190b99716900
Volkan Tagal <volkan.tagal@hepsiburada.com>
2016-05-24 10:33:31 +0300

    Form Elements Kategorisi oluşturuldu, Colors ve Typography & Icons kategorileri pug olarak tekrar kodlandı.

---
+++ test/components/buttons.js
@@ -0,0 +1,35 @@
+var phantomcss = require('phantomcss');
+phantomcss.init({
+  screenshotRoot: './phantomcss/screenshot',
+  failedComparisonsRoot: './phantomcss/failures',
+  comparisonResultRoot: './phantomcss/results'
+});
+
+casper.test.begin('Form elements testing', 1, function (test) {
+  casper.on('error', function (err) {
+    this.die('PhantomJS has errored: ' + err);
+  });
+
+  casper.on('resource.error', function (err) {
+    casper.log('Resource load error: ' + err, 'warning');
+  });
+
+  casper.start('http://localhost:3000/pages/form-elements.html');
+
+  casper.viewport(1024, 768);
+
+  casper.then(function () {
+    phantomcss.screenshot('#smallButtons .hb-button.normal', 'hb-button-normal');
+  });
+
+  casper.then(function () {
+    phantomcss.compareAll();
+  });
+
+  casper.run(function () {
+    phantomcss.getExitStatus();
+    test.done();
+    casper.test.done();
+    casper.exit();
+  });
+});
